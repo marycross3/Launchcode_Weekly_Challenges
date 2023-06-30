ef3adcd254144281357b48f322e48cb5dd3bd2b3
marycross3 <marycradick@gmail.com>
2023-06-29 19:05:14 -0500

    week one done

---
+++ Week1_Challenge.js
@@ -0,0 +1,10 @@
+
+let area;
+
+function calcArea (base, height){
+    return area = (base * height)/2
+};
+let height = 6
+let base = 10
+console.log("The area of a rectangle with a height of " + height + 
+" and base of " + base+ " is " + calcArea(base, height) + ".")
