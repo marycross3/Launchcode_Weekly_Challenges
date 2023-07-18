f4a37b1a565bcad776263a98629ea1365a85c804
marycross3 <marycradick@gmail.com>
2023-07-18 14:13:22 -0500

    finished

--- Week5_Challenge.js
+++ Week5_Challenge.js
@@ -1,5 +1,21 @@
-// Create a method in the Person class which returns how another person's age compares. Given the 
-// instances p1, p2 and p3, which will be initialised with the attributes name and age, return a 
-// sentence in the following format: {other person name} is {older than / younger than / the same age as} me
 
-//https://github.com/marycross3/Launchcode_Weekly_Challenges.git
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		if (this.age < other.age){
+			return `${other.name} is older than me.`
+		}else if (this.age > other.age){
+			return `${other.name} is younger than me.`
+		}else{
+			return `${other.name} is the same age as me.`
+		}
+	}
+}
+	let p1 = new Person("Samuel", 24)
+	let p2 = new Person("Joel", 36);
+	let p3 = new Person("Lily", 24);
+console.log(p1.compareAge(p2));
